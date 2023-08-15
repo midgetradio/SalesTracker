980d83efe3a69d67e95e2a9ac152811c19fff2fc
midgetradio <midgetradio@gmail.com>
2023-08-14 17:47:19 -0700

    clean up

--- SalesTracker/SalesTracker/Program.cs
+++ SalesTracker/SalesTracker/Program.cs
@@ -7,8 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-
-var replaced = EnvironmentVariableReplacer.Replace(connectionString);
+connectionString = EnvironmentVariableReplacer.Replace(connectionString);
 
 builder.Services.AddDbContext<SalesTrackerDBContext>(options =>
     options.UseSqlServer(connectionString));
