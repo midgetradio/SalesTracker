3eb0e3f3288aefa10654085a0054870c7a2ffd9d
Matt McLain <midgetradio@gmail.com>
2024-01-07 09:41:12 -0700

    only show dates where titles have been added on home page

--- SalesTracker/SalesTracker/Controllers/HomeController.cs
+++ SalesTracker/SalesTracker/Controllers/HomeController.cs
@@ -22,7 +22,7 @@ namespace SalesTracker.Controllers
         public IActionResult Index(string date, int index)
         {
             var model = new HomeVM();
-            model.Dates = _context.Editions.Select(s => s.LastUpdated.Date).Distinct().Take(20).ToList();
+            model.Dates = _context.Editions.Where(w => w.IsDeleted == false).Select(s => s.LastUpdated.Date).Distinct().Take(20).ToList();
             model.Dates = model.Dates.OrderBy(o => o.Date).ToList();
 
             if(String.IsNullOrEmpty(date) || date == "All Time")
